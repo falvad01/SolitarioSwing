3b44d1a3da10d016329431754ee23895dd3e8cad
falvad01 <falvad01@estudiantes.unileon.es>
2018-12-04 22:51:49 +0100

    Interfaz cambiada, empezados los submenus

--- src/es/falvad01/unileon/Solitario/VentanaPrincipal.java
+++ src/es/falvad01/unileon/Solitario/VentanaPrincipal.java
@@ -3,6 +3,7 @@ package es.falvad01.unileon.Solitario;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,16 +13,17 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
 import javax.swing.JLabel;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JComboBox;
 
 public class VentanaPrincipal extends JFrame implements ActionListener {
 
+	Toolkit screen;
 	private int screenHeight;
 	private int screenWith;
-	private int panelHeight;
-	private int panelWith;
 
-	private int buttonW = 128;
-	private int buttonH = 62;
+	private JPanel menu;
 	private JPanel archivo;
 	private JPanel editar;
 	private JPanel historial;
@@ -33,25 +35,36 @@ public class VentanaPrincipal extends JFrame implements ActionListener {
 	private JButton btnEditar;
 	private JButton btnHistorial;
 	private JButton btnAyuda;
+
 	private JLabel lblEditar;
 	private JLabel lblHistorial;
 	private JLabel lblAyuda;
-	private JLabel lblAyuda_1;
+	
+	
+	JComboBox comboBox;
+	String gameSelected;
+	private JButton btnNewButton_4;
+	
+	
 
 	public VentanaPrincipal() {
 
-		Toolkit screen = Toolkit.getDefaultToolkit();
+		screen = Toolkit.getDefaultToolkit();
 		Dimension screenSize = screen.getScreenSize();
 		screenHeight = screenSize.height;
 		screenWith = screenSize.width;
-		panelWith = screenWith / 2;
-		panelHeight = screenHeight / 2;
 
-		setBounds(panelWith / 2, panelHeight / 2, panelWith, panelHeight);
+		// setBounds(panelWith / 2, panelHeight / 2, panelWith, panelHeight);
+
+		setBounds(1024 / 4, 768 / 4, 650, 550);
 
 		setTitle("Solitario");
 
 		setResizable(false);
+
+		Image icono = screen.getImage("etc/images/interface/icono.jpg");
+
+		setIconImage(icono);
 
 		initComponents();
 
@@ -60,59 +73,53 @@ public class VentanaPrincipal extends JFrame implements ActionListener {
 	private void initComponents() {
 
 		getContentPane().setLayout(null);
-
-		ayuda = new JPanel();
-		ayuda.setBackground(Color.MAGENTA);
-		ayuda.setBounds(10, 142, 614, 330);
-		getContentPane().add(ayuda);
-		ayuda.setLayout(null);
+		Image backGround = screen.getImage("etc/images/interface(backGround.jpg");
 		
+		archivo = new JPanel();
+		archivo.setBackground(Color.YELLOW);
+		archivo.setBounds(10, 0, 624, 510);
+		getContentPane().add(archivo);
+		archivo.setLayout(null);
+		archivo.setVisible(false);
+		
+				
+				
+				
+
 		
 
-		historial = new JPanel();
-		historial.setBackground(Color.CYAN);
-		historial.setBounds(10, 142, 614, 330);
-		getContentPane().add(historial);
-		historial.setLayout(null);
+		
+
+		/////////////////////////// PANELES/////////////////////////////////////////
+
+		menu = new JPanel();
+		menu.setBackground(null);
+		menu.setBounds(10, 0, 624, 510);
+		getContentPane().add(menu);
+		menu.setLayout(null);
+		menu.setVisible(true);
 
 		editar = new JPanel();
 		editar.setBackground(Color.BLUE);
-		editar.setBounds(10, 142, 614, 330);
+		editar.setBounds(10, 0, 624, 510);
 		getContentPane().add(editar);
 		editar.setLayout(null);
 
-		/////////////////////////// PANELES/////////////////////////////////////////
-		archivo = new JPanel();
-		archivo.setBackground(Color.YELLOW);
-		archivo.setBounds(10, 142, 614, 330);
-		getContentPane().add(archivo);
-		archivo.setLayout(null);
-		archivo.setVisible(true);
+		historial = new JPanel();
+		historial.setBackground(Color.CYAN);
+		historial.setBounds(10, 0, 624, 510);
+		getContentPane().add(historial);
+		historial.setLayout(null);
 
-///////////////////////////BOTONES/////////////////////////////////////////
+		ayuda = new JPanel();
+		ayuda.setBackground(Color.MAGENTA);
+		ayuda.setBounds(10, 0, 624, 510);
+		getContentPane().add(ayuda);
+		ayuda.setLayout(null);
 
-		btnArchivo = new JButton("Archivo");
-		btnArchivo.setBounds(10, 32, buttonW, buttonH);
-		getContentPane().add(btnArchivo);
-		btnArchivo.addActionListener(this);
 
-		btnEditar = new JButton("Editar");
-		btnEditar.setBounds(163, 32, buttonW, buttonH);
-		getContentPane().add(btnEditar);
-		btnEditar.addActionListener(this);
 
-		btnHistorial = new JButton("Historial");
-		btnHistorial.setBounds(336, 32, buttonW, buttonH);
-		getContentPane().add(btnHistorial);
-		btnHistorial.addActionListener(this);
-
-		btnAyuda = new JButton("Ayuda");
-		btnAyuda.setBounds(496, 32, buttonW, buttonH);
-		getContentPane().add(btnAyuda);
-		btnAyuda.addActionListener(this);
-		
-		
-		
+		initMenuComponents();
 		initArchivoComponents(); // Llamamos a los componentes de cada panel
 		initEditarComponents();
 		initHistorialComponenets();
@@ -120,12 +127,75 @@ public class VentanaPrincipal extends JFrame implements ActionListener {
 
 	}
 
-	private void initArchivoComponents() {
+	private void initMenuComponents() {
 
-		JLabel lblArchivo = new JLabel("ARCHIVO");
-		lblArchivo.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblArchivo.setBounds(10, 264, 165, 66);
-		archivo.add(lblArchivo);
+		btnArchivo = new JButton("Archivo");
+		btnArchivo.setBounds(137, 55, 315, 62);
+		menu.add(btnArchivo);
+		btnArchivo.addActionListener(this);
+
+		btnEditar = new JButton("Editar");
+		btnEditar.setBounds(137, 170, 315, 62);
+		menu.add(btnEditar);
+		btnEditar.addActionListener(this);
+
+		btnHistorial = new JButton("Historial");
+		btnHistorial.setBounds(137, 277, 315, 62);
+		menu.add(btnHistorial);
+		btnHistorial.addActionListener(this);
+
+		btnAyuda = new JButton("Ayuda");
+		btnAyuda.setBounds(137, 391, 315, 62);
+		menu.add(btnAyuda);
+		btnAyuda.addActionListener(this);
+
+	}
+
+	private void initArchivoComponents() {
+		
+		
+		JButton atras = new JButton("Atras");
+		atras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				menu.setVisible(true);
+				archivo.setVisible(false);
+				editar.setVisible(false);
+				historial.setVisible(false);
+				ayuda.setVisible(false);
+
+				setTitle("Solitario");
+			}
+		});
+		atras.setBounds(10, 11, 112, 37);
+		archivo.add(atras);
+		
+		
+		 comboBox = new JComboBox();
+		comboBox.setBounds(43, 124, 122, 20);
+		archivo.add(comboBox);
+		comboBox.addItem("Saltos");
+		comboBox.addItem("Clasico");
+		
+		JButton btnNewButton = new JButton("Nuevo");
+		btnNewButton.setBounds(214, 123, 157, 23);
+		archivo.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Cargar");
+		btnNewButton_1.setBounds(214, 181, 157, 23);
+		archivo.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Salvar");
+		btnNewButton_2.setBounds(214, 243, 157, 23);
+		archivo.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Salvar como");
+		btnNewButton_3.setBounds(214, 318, 157, 23);
+		archivo.add(btnNewButton_3);
+		
+		btnNewButton_4 = new JButton("Salir");
+		btnNewButton_4.setBounds(200, 409, 171, 23);
+		archivo.add(btnNewButton_4);
+		comboBox.addActionListener(this);
 		
 
 	}
@@ -139,6 +209,22 @@ public class VentanaPrincipal extends JFrame implements ActionListener {
 		editar.add(lblEditar);
 		editar.setVisible(false);
 
+		JButton atras = new JButton("Atras");
+		atras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				menu.setVisible(true);
+				archivo.setVisible(false);
+				editar.setVisible(false);
+				historial.setVisible(false);
+				ayuda.setVisible(false);
+				
+				setTitle("Solitario");
+
+			}
+		});
+		atras.setBounds(10, 11, 112, 37);
+		editar.add(atras);
+
 	}
 
 	private void initHistorialComponenets() {
@@ -147,13 +233,45 @@ public class VentanaPrincipal extends JFrame implements ActionListener {
 		lblHistorial.setBounds(20, 231, 171, 88);
 		historial.add(lblHistorial);
 
+		JButton atras = new JButton("Atras");
+		atras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				menu.setVisible(true);
+				archivo.setVisible(false);
+				editar.setVisible(false);
+				historial.setVisible(false);
+				ayuda.setVisible(false);
+				
+				setTitle("Solitario");
+
+			}
+		});
+		atras.setBounds(10, 11, 112, 37);
+		historial.add(atras);
+
 	}
 
 	private void initAyudaComponents() {
-		lblAyuda_1 = new JLabel("AYUDA");
-		lblAyuda_1.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblAyuda_1.setBounds(10, 200, 200, 130);
-		ayuda.add(lblAyuda_1);
+		lblAyuda = new JLabel("AYUDA");
+		lblAyuda.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblAyuda.setBounds(10, 200, 200, 130);
+		ayuda.add(lblAyuda);
+
+		JButton atras = new JButton("Atras");
+		atras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				menu.setVisible(true);
+				archivo.setVisible(false);
+				editar.setVisible(false);
+				historial.setVisible(false);
+				ayuda.setVisible(false);
+				
+				setTitle("Solitario");
+
+			}
+		});
+		atras.setBounds(10, 11, 112, 37);
+		ayuda.add(atras);
 
 	}
 
@@ -161,29 +279,51 @@ public class VentanaPrincipal extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == btnArchivo) {
+			menu.setVisible(false);
 			archivo.setVisible(true);
 			editar.setVisible(false);
 			historial.setVisible(false);
 			ayuda.setVisible(false);
+			
+			setTitle("Solitario-Archivo");
 
 		} else if (e.getSource() == btnEditar) {
+
+			menu.setVisible(false);
 			archivo.setVisible(false);
 			editar.setVisible(true);
 			historial.setVisible(false);
 			ayuda.setVisible(false);
+			
+			setTitle("Solitario-Editar");
 
-		}else if(e.getSource() == btnHistorial) {
+		} else if (e.getSource() == btnHistorial) {
+
+			menu.setVisible(false);
 			archivo.setVisible(false);
 			editar.setVisible(false);
 			historial.setVisible(true);
 			ayuda.setVisible(false);
+			
+			setTitle("Solitario-Historial");
 
-		}else if(e.getSource() == btnAyuda) {
+		} else if (e.getSource() == btnAyuda) {
+
+			menu.setVisible(false);
 			archivo.setVisible(false);
 			editar.setVisible(false);
 			historial.setVisible(false);
 			ayuda.setVisible(true);
+			
+			setTitle("Solitario-Ayuda");
 
+		}
+		
+		
+		if(e.getSource() == comboBox) {
+			
+			gameSelected = (String) comboBox.getSelectedItem();
+			
 		}
 
 	}
