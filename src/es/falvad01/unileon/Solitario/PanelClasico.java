ac0f8884c8aaa7d058d6e91d0fdbf10341e1d312
falvad01 <falvad01@estudiantes.unileon.es>
2018-12-25 12:53:43 +0100

    rellenos de arrays

--- src/es/falvad01/unileon/Solitario/PanelClasico.java
+++ src/es/falvad01/unileon/Solitario/PanelClasico.java
@@ -51,20 +51,20 @@ public class PanelClasico extends JPanel {
 		this.clasico = panel;
 		this.baraja = new Baraja(EJuego.Clasico);
 		barajaInicial = new JButton[23];
-		barajaInicial = new JButton[24];
+		barajaDescartes = new JButton[24];
 
 		fin1 = new JButton[13];
 		fin2 = new JButton[13];
 		fin3 = new JButton[13];
 		fin4 = new JButton[13];
 
-		monton1 = new JButton[1];
-		monton2 = new JButton[2];
-		monton3 = new JButton[3];
-		monton4 = new JButton[4];
-		monton5 = new JButton[5];
-		monton6 = new JButton[6];
-		monton7 = new JButton[7];
+		monton1 = new JButton[52];
+		monton2 = new JButton[52];
+		monton3 = new JButton[52];
+		monton4 = new JButton[52];
+		monton5 = new JButton[52];
+		monton6 = new JButton[52];
+		monton7 = new JButton[52];
 
 	}
 
@@ -83,28 +83,93 @@ public class PanelClasico extends JPanel {
 
 		System.out.println("/n");
 
+		pintarCartas();
+
 	}
 
 	private void pintarCartas() {
 
 		StringBuilder nombre = new StringBuilder();
+		int i1 = 0;
+		int i2 = 0;
+		int i3 = 0;
+		int i4 = 0;
+		int i5 = 0;
+		int i6 = 0;
+		int i7 = 0;
 
-		for (int i = 0; i < 23; i++) {
+		for (int i = 0; i < 52; i++) {
 
 			nombre.append(ABaraja[i]);
 
 			Image img = ABaraja[i].getImagen().getImage();
 
 			img = img.getScaledInstance(95, 120, Image.SCALE_SMOOTH);
-			
+
 			ImageIcon icono = new ImageIcon(img);
-			
-			if(i < 23) {
+
+			if (i < 23) {
 				barajaInicial[i] = new JButton(nombre.toString());
-			}
+				// System.out.println(nombre.toString());
+				System.out.println(i);
+
+			} else if (i == 23) {
+				
+				barajaDescartes[0] = new JButton(nombre.toString());
+				//System.out.println(nombre.toString());
+				System.out.println(i);
+				
+			} else if (i == 24) {
+				
+				monton1[i1] = new JButton(nombre.toString());
+			//	System.out.println(nombre.toString());
+				System.out.println(i);
+				i1++;
+				
+			} else if (i > 24 && i < 27) {
+				
+				monton2[i2] = new JButton(nombre.toString());
+				System.out.println("f2-" + i2);
+				i2++;
+				System.out.println(i);
+				
+			} else if (i >= 28 && i <= 30) {
+				
+				monton3[i3] = new JButton(nombre.toString());
+				System.out.println("f3-" + i3);
+				i3++;
+				System.out.println(i);
+				
+			} else if (i >= 31 && i <= 34) {
+				
+				monton4[i4] = new JButton(nombre.toString());
+				System.out.println("f4-" + i4);
+				i4++;
+				System.out.println(i);
+				
+			} else if (i >= 35 && i <= 39) {
+				
+				monton5[i5] = new JButton(nombre.toString());
+				System.out.println("f5-" + i5);
+				i5++;
+				System.out.println(i);
+				
+			} else if (i >= 40 && i <= 45) {
+				
+				monton6[i6] = new JButton(nombre.toString());
+				System.out.println("f6-" + i6);
+				i6++;
+				System.out.println(i);
+				
+			} else if (i >= 46 && i < 52) {
+				//TODO FALLA AQUI, NO SUBE A F7-6
+				monton7[i7] = new JButton(nombre.toString());
+				System.out.println("f7-" + i7);
+				i7++;
+				System.out.println(i);
+				
+			} 
 		}
-		
-		
 
 	}
 
